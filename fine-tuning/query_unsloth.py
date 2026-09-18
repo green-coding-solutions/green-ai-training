163bcbf2d55a53350567b3f8329fc1d1e8b1331a
Arne Tarara <arne@datafuse.de>
2026-09-18 08:59:25 +0200

    Using PT model tensors to better load LoRA trained models

--- fine-tuning/query_unsloth.py
+++ fine-tuning/query_unsloth.py
@@ -2,7 +2,7 @@ import argparse
 
 from unsloth import FastLanguageModel
 import torch
-import mlx.core as mx
+from peft import PeftModel
 
 # unsloth/Qwen2.5-Coder-0.5B-Instruct any good?
 #    - Quick. Takes 0:16
@@ -19,10 +19,8 @@ def generate_output(model_name, prompt, lora_adapter_folder):
         model_name=model_name,
         max_seq_length=4096,
         load_in_4bit=True,
+        adapter_name=lora_adapter_folder,
     )
-
-    if lora_adapter_folder:
-        model.load_adapter(lora_adapter_folder)
 
     # Optimize the model for inference
     FastLanguageModel.for_inference(model)
@@ -36,17 +34,17 @@ def generate_output(model_name, prompt, lora_adapter_folder):
             messages,
             tokenize=True,
             add_generation_prompt=True,
-            return_tensors="np",
+            return_tensors="pt",
         )
 
     else:
         print("Model has no Chat Template configured. ", "\n", "Falling back to direct prompt.")
 
-        inputs = tokenizer(prompt, return_tensors="np")["input_ids"]
+        inputs = tokenizer(prompt, return_tensors="pt")["input_ids"]
 
     with torch.inference_mode():
         outputs = model.generate(
-            input_ids=mx.array(inputs),
+            input_ids=inputs,
             max_new_tokens=8192,
             do_sample=False,
             # If more output variation is wanted temperature can be set, but top_p should
